c8ac6605456ee774e93e23c863d5bb9754ebc898
Yao Shan <yao.shan@student.unimelb.edu.au>
2022-08-26 14:02:35 +1000

    added footer to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
 import Welcome from "./components/pages/Welcome";
+import Footer from "./components/Layout/Footer.js";
 
 function App() {
   return (
     <Fragment>
        <Welcome/>
+       <Footer/>
     </Fragment>
   );
 }
