e2c4b1c364f16c0d2ff3e55dd59ff40635df850d
Andy Low <andyjunchenglow@gmail.com>
2022-08-26 16:22:41 +1000

    Added changes to header and footer, tweaked welcome body

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,9 +1,22 @@
-import classes from "./Header.module.css"
+import classes from "./Header.module.css";
 
 function Header() {
-    return <div className={classes.header}>
-        <h1>COACHMATE</h1>
+  return (
+    <div className={classes.header}>
+      <h1>COACHMATE</h1>
+      <nav>
+        <ul className={classes["flexbox-navbar-text"]}>
+          <li>
+            <a>ABOUT US</a>
+          </li>
+          <li>
+            <a>CONTACT</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={classes["login"]}>LOGIN</div>
     </div>
+  );
 }
 
 export default Header;
