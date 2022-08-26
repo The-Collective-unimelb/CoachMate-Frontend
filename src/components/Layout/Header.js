7ccae4a40c7145aa4b4610fe5f2f11cf7fc68f65
ChengYin <loh.chengyin@gmail.com>
2022-08-26 11:31:16 +1000

    cleanup

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,15 +1,8 @@
 import classes from "./Header.module.css"
 
-const headerStyle = {
-    font: "Roboto",
-    fontSize: 36,
-    marginLeft: 50,
-    fontWeight: "bold"
-
-}
 function Header() {
     return <div className={classes.header}>
-        <h1 style={headerStyle}>COACHMATE</h1>
+        <h1>COACHMATE</h1>
     </div>
 }
 
---
+++ src/components/pages/Welcome.js
@@ -0,0 +1,7 @@
+import Header from "../Layout/Header";
+
+function Welcome() {
+    return <div><Header/></div>
+}
+
+export default Welcome;
