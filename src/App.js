37886102fb6ec724f637fcc61b85ccb0e626867b
Andy Low <andyjunchenglow@gmail.com>
2022-08-26 14:46:44 +1000

    Partially finished welcome body

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
+import Header from "./components/Layout/Header";
 import Welcome from "./components/pages/Welcome";
 
 function App() {
   return (
     <Fragment>
+       <Header/>
        <Welcome/>
     </Fragment>
   );
--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -1,7 +1,31 @@
 import Header from "../Layout/Header";
+import classes from "./Welcome.module.css";
+import Card from "../UI/Card";
+import netball from "./netball.png";
 
 function Welcome() {
-    return <div><Header/></div>
+  return (
+    <div className={classes["vertical-flex"]}>
+      <div className={classes["horizontal-flex"]}>
+        <div className={classes["welcome-quote"]}>
+          LOREM IPSUM DOLOR SIT AMET
+          <div className={classes["welcome-quote-small"]}>
+            Lorem ipsum dolor sit amet. Vel placeat autem et debitis numquam 33
+            delectus quod et totam voluptate. Quo blanditiis unde ut dolor
+            expedita est quidem vitae.
+          </div>
+        </div>
+        <Card>
+          <img className={classes["netball-img"]} src={netball}></img>
+          <div className={classes["card-title"]}>NETBALL</div>
+          <div className={classes["card-quote"]}>
+            Lorem ipsum dolor sit amet. Vel placeat autem et debitis numquam 33
+            delectus quod
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
