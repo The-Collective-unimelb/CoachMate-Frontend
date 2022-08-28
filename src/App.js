e8948f12af30ebe808f75d46bf0d2adef386d4dd
Andy Low <andyjunchenglow@gmail.com>
2022-08-29 04:11:57 +1000

    Changed logo and added coach profile skeleton

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import {Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import NoPage from "./components/pages/NoPage";
 import Welcome from "./components/pages/Welcome";
 import Footer from "./components/Layout/Footer.js";
 import Header from "./components/Layout/Header";
+import CoachProfile from "./components/pages/CoachProfile";
 
 function App() {
   return (
     <div>
-      <Header />
+      <Header/>
       <Routes>
         <Route path="/" element={<Welcome/>}/>
-        <Route path="/about-us"  element={<NoPage/>}/>
+        // FOR PAGE MAKING PURPOSES
+        <Route path="/about-us"  element={<CoachProfile/>}/>
         <Route path="/contact"  element={<NoPage/>}/>
         <Route path="/login"  element={<NoPage/>}/>
       </Routes>
