e2bc032eb7a6b6c438a42dfb401f15f18bab59d3
ChengYin <loh.chengyin@gmail.com>
2022-08-29 22:24:04 +1000

    dynamically render coach cards

--- src/components/pages/CoachesList.js
+++ src/components/pages/CoachesList.js
@@ -1,18 +1,35 @@
+import Button from "../UI/Button";
 import CoachCard from "../UI/CoachCard";
 import classes from "./CoachesList.module.css";
+
+const DUMMY_VARS = [
+  { name: "John", avail: "9/9/22", location: "mel" },
+  { name: "Jack", avail: "10/10/22", location: "mel" },
+  { name: "James", avail: "11/11/22", location: "mel" },
+  { name: "Jamie", avail: "12/12/22", location: "mel" }
+];
 
 function CoachesList() {
   return (
     <div>
       <form className={classes.form}>
         {/* <label htmlFor="search">Search Coach: </label> */}
-        <input id="search" type="text" placeholder="Enter Name" />
-        <button>Filter</button>
-        <button>Search</button>
+        <input id="search" type="text" placeholder="Search..." />
+        <div className={classes.buttons}>
+          <Button>Filter</Button>
+          <Button>Search</Button>
+        </div>
       </form>
       <section className={classes["coach-cards"]}>
-        <CoachCard />
-        <CoachCard />
+        {DUMMY_VARS.map((coach) => {
+          return (
+            <CoachCard
+              name={coach.name}
+              avail={coach.avail}
+              location={coach.location}
+            />
+          );
+        })}
       </section>
     </div>
   );
