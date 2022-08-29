3cc6e1b7a38060d28d61644edbc5cfe6a5f2fd52
ChengYin <loh.chengyin@gmail.com>
2022-08-29 22:56:16 +1000

    update coach lists

--- src/components/UI/CoachCard.js
+++ src/components/UI/CoachCard.js
@@ -4,9 +4,10 @@ import testPic from "../../assets/profile pic.png";
 function CoachCard(props) {
     return (<div className={classes["coach-cards"]}>
         <img className={classes.pic} src={testPic} alt="test profile pic"/>
+        <div>
         <h2>{props.name}</h2>
-        <p>{props.avail}</p>
-        <h3>{props.location}</h3>
+        <p>{props.avail}</p></div>
+        <h4>{props.location}</h4>
     </div>);
 
 }
