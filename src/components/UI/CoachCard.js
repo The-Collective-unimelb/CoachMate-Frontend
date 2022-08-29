4a7d27e9b9cdf112c4e4778c2acb6bab30dc8385
ChengYin <loh.chengyin@gmail.com>
2022-08-29 22:21:49 +1000

    added props

--- src/components/UI/CoachCard.js
+++ src/components/UI/CoachCard.js
@@ -1,12 +1,12 @@
 import classes from "./CoachCard.module.css"
 import testPic from "../../assets/profile pic.png";
 
-function CoachCard() {
+function CoachCard(props) {
     return (<div className={classes["coach-cards"]}>
         <img className={classes.pic} src={testPic} alt="test profile pic"/>
-        <h2>Name</h2>
-        <p>Next availability</p>
-        <h3>Location</h3>
+        <h2>{props.name}</h2>
+        <p>{props.avail}</p>
+        <h3>{props.location}</h3>
     </div>);
 
 }
