67ff98479a4c265745a4952574a045ceb1735b9a
Andy Low <andyjunchenglow@gmail.com>
2022-09-09 12:08:16 +1000

    First push to Heroku

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import NoPage from "./components/pages/NoPage";
 import Welcome from "./components/pages/Welcome";
 import Footer from "./components/Layout/Footer.js";
@@ -13,18 +13,18 @@ import CoachDashboard from "./components/pages/CoachDashboard";
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Welcome/>}/>
+        <Route path="/" element={<Welcome />} />
         {/* FOR PAGE MAKING PURPOSES */}
-        <Route path="/coach-profile"  element={<CoachProfile/>}/>
-        <Route path="/edit-profile"  element={<EditProfile/>}/>
-        <Route path="/coach-dashboard"  element={<CoachDashboard/>}/>
-        <Route path="/login"  element={<LoginForm/>}/>
-        <Route path="/coaches"  element={<CoachesList/>}/>
-        <Route path="/signup" element={<SignUpForm/>}/>
+        <Route path="/coach-profile" element={<CoachProfile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/coach-dashboard" element={<CoachDashboard />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/coaches" element={<CoachesList />} />
+        <Route path="/signup" element={<SignUpForm />} />
       </Routes>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
