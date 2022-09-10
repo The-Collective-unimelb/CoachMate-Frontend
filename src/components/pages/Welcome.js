c5e2589c714b5a6797396c44586b7acebf745753
Andy Low <andyjunchenglow@gmail.com>
2022-09-11 03:32:36 +1000

    Added search bar to welcome screen

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -1,11 +1,18 @@
 import classes from "./Welcome.module.css";
 import Card from "../UI/Card";
 import netball from "../../assets/netball.png";
+import { TextField, Button } from "@mui/material";
 
 function Welcome() {
   return (
     <div className={classes["vertical-flex"]}>
-      <div className={classes["horizontal-flex"]}>
+      <div className={classes["search-title"]}>SEARCH COACHMATE</div>
+      <div className={classes["horizontal-search-flex"]}>
+        <TextField id="standard-basic" label="SPORT" variant="standard" />
+        <TextField id="standard-basic" label="LOCATION" variant="standard" />
+        <Button variant="contained">FIND COACH</Button>
+      </div>
+      <div className={classes["horizontal-content-flex"]}>
         <div className={classes["welcome-quote"]}>
           LOREM IPSUM DOLOR SIT AMET
           <div className={classes["welcome-quote-small"]}>
@@ -25,6 +32,7 @@ function Welcome() {
             Lorem ipsum dolor sit amet. Vel placeat autem et debitis numquam 33
             delectus quod
           </div>
+          <Button variant="contained">SEE MORE</Button>
         </Card>
       </div>
     </div>
