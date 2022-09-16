efd045c55793241504e1bbc53e2385d1c634c943
ChengYin <loh.chengyin@gmail.com>
2022-09-16 23:09:12 +1000

    add schedule route

--- src/App.js
+++ src/App.js
@@ -9,21 +9,23 @@ import EditProfile from "./components/pages/EditProfile";
 import LoginForm from "./components/pages/Login";
 import SignUpForm from "./components/pages/SignUp";
 import CoachDashboard from "./components/pages/CoachDashboard";
+import CoachSchedule from "./components/pages/CoachSchedule";
 
 function App() {
   return (
     <div>
       <Header />
       <Routes>
-          <Route path="/" element={<Welcome />} />
-          {/* FOR PAGE MAKING PURPOSES */}
-          <Route path="/coach-profile" element={<CoachProfile />} />
-          <Route path="/edit-profile" element={<EditProfile />} />
-          <Route path="/coach-dashboard" element={<CoachDashboard />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/coaches" element={<CoachesList />} />
-          <Route path="/signup" element={<SignUpForm />} />
-          <Route path="/*" element={<NoPage />} />
+        <Route path="/" element={<Welcome />} />
+        {/* FOR PAGE MAKING PURPOSES */}
+        <Route path="/coach-profile" element={<CoachProfile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
+        <Route path="/coach-dashboard" element={<CoachDashboard />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/coaches" element={<CoachesList />} />
+        <Route path="/signup" element={<SignUpForm />} />
+        <Route path="/schedule" element={<CoachSchedule />} />
+        <Route path="/*" element={<NoPage />} />
       </Routes>
       <Footer />
     </div>
