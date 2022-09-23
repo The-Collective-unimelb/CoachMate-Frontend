118c3476f012bdc5f0f11589bfd988cd462b4baa
ChengYin <loh.chengyin@gmail.com>
2022-09-24 02:24:26 +1000

    add onclick prop

--- src/components/UI/CoachCard.js
+++ src/components/UI/CoachCard.js
@@ -1,15 +1,17 @@
-import classes from "./CoachCard.module.css"
+import classes from "./CoachCard.module.css";
 import testPic from "../../assets/profile pic.png";
 
 function CoachCard(props) {
-    return (<div className={classes["coach-cards"]}>
-        <img className={classes.pic} src={testPic} alt="test profile pic"/>
-        <div>
+  return (
+    <div className={classes["coach-cards"]} onClick={props.onClick}>
+      <img className={classes.pic} src={testPic} alt="test profile pic" />
+      <div>
         <h2>{props.name}</h2>
-        <p>{props.avail}</p></div>
-        <h4>{props.location}</h4>
-    </div>);
-
+        <p>{props.avail}</p>
+      </div>
+      <h4>{props.location}</h4>
+    </div>
+  );
 }
 
 export default CoachCard;
