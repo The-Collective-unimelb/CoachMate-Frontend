db57f171a356b050a99f3005ac5e846eaa578b4e
ChengYin <loh.chengyin@gmail.com>
2022-09-24 15:18:56 +1000

    style page; add desc and link

--- src/components/pages/NoPage.js
+++ src/components/pages/NoPage.js
@@ -1,5 +1,15 @@
-function NoPage() {
-    return <h1>404</h1>
+import { Link } from "react-router-dom";
+import classes from "./NoPage.module.css";
+
+function NoPage(props) {
+    const linkText = <Link to={props.to} style={{color:"dodgerblue"}}>{props.linkText}</Link>
+  return (
+    <div className={classes.page}>
+      <h1>404</h1>
+      <h3>{props.text} {linkText}</h3>
+
+    </div>
+  );
 }
 
 export default NoPage;
